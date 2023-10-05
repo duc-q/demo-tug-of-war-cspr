f1100d23b7bae503070a6acf7863151e3e6aaea1
Duc Quach <duc.quach@groovetechnology.com>
2023-10-05 08:02:14 +0700

    UPDATE: - Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useMemo, useState } from 'react'
+import useWebSocket, { ReadyState } from 'react-use-websocket'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const url = "wss://stream.binance.com:9443/ws/btcusdt@kline_1s";
+  //Public API that will echo messages sent to it back to the client
+
+  const [isStarted, setStarted] = useState(false);
+  const [socketUrl, setSocketUrl] = useState(url);
+  const [messageHistory, setMessageHistory] = useState([]);
+
+  const { sendMessage, lastMessage, readyState } = useWebSocket(isStarted ? socketUrl : null);
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  }[readyState];
+
+  const data = useMemo(() => {
+    if (lastMessage) {
+      const result = JSON.parse(lastMessage.data)
+      console.log(`🚀 ~ data ~ result:`, result)
+      return result.p;
+    }
+    return null;
+  }, [lastMessage]);
+
+  
+
+  console.log(`🚀 ~ App ~ lastMessage:`, lastMessage?.data)
+  // console.log(`🚀 ~ App ~ readyState:`, readyState)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h3>
+          <strong>{data}</strong>
+        </h3>
       </div>
-      <h1>Vite + React</h1>
+      {/* <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -27,7 +51,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <button onClick={() => setStarted(true)}>Start</button>
+      <button onClick={() => setStarted(false)}>Stop</button>
     </>
   )
 }
